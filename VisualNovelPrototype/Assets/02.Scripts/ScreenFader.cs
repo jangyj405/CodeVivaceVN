184a63b225cde623777547a402a28756a815bcc1
jangyj405 <jangyj405@gmail.com>
2019-01-10 17:31:01 +0900

    DoTween Import, Change Settings
    need to Update Unity to Latest Version

---
+++ VisualNovelPrototype/Assets/02.Scripts/ScreenFader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class ScreenFader : MonoBehaviour
+{
+    //for using this script as singletone
+    public static ScreenFader instance = null;
+
+    //Fading Image
+    [SerializeField]
+    protected Image fdImage = null;
+    public void FadeIn(float _time)
+    {
+        fdImage.gameObject.SetActive(true);
+        Tweener tTween = fdImage.DOFade(0f, _time);
+        tTween.OnComplete(() => { fdImage.gameObject.SetActive(false); });
+    }
+    public void FadeOut(float _time)
+    {
+        fdImage.gameObject.SetActive(true);
+        Tweener tTween = fdImage.DOFade(1f, _time);
+        tTween.OnComplete(() => { fdImage.gameObject.SetActive(false); });
+    }
+    public void BlackOutScreen()
+    {
+        fdImage.color = Color.black;
+    }
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+ 
+}
